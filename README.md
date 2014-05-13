8f6f71ead5a492a024cab4824799ad0b147fcbe8
moznion <moznion@gmail.com>
2014-05-13 21:01:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Parse::KeyValue::Shellish - It's new $module
+
+# SYNOPSIS
+
+    use Parse::KeyValue::Shellish;
+
+# DESCRIPTION
+
+Parse::KeyValue::Shellish is ...
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/Parse/KeyValue/Shellish.pm
@@ -0,0 +1,39 @@
+package Parse::KeyValue::Shellish;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Parse::KeyValue::Shellish - It's new $module
+
+=head1 SYNOPSIS
+
+    use Parse::KeyValue::Shellish;
+
+=head1 DESCRIPTION
+
+Parse::KeyValue::Shellish is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
